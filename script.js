8fdcd5a96fcf1ef3dfa1b436f34882c439e9105e
Gianpietro Consiglio <83292980+Gianpietro-Consiglio@users.noreply.github.com>
2024-01-11 21:17:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let botao_calcular = window.document.getElementById('button')
+let botao_limpar = window.document.getElementById('button2')
+botao_calcular.addEventListener('click',calcular)
+botao_limpar.addEventListener('click',limpar)
+
+function calcular(){
+    let peso = window.document.getElementById('peso').value
+    let altura = window.document.getElementById('altura').value
+    let imc = peso / (altura * altura)   
+    let diagnostico = ''
+    if (String(peso).length == 0 || String(altura).length == 0){
+        alert('Preencha todos os campos!')
+        return
+    }else if (String(altura).length != 4){
+        alert('Preencha o campo altura em METROS. Exemplo: 1.70')
+        return
+    }
+
+    if (imc < 18.5){
+        diagnostico = 'Baixo peso'
+    }else if ( imc > 18.5 && imc <= 24.9 ){
+        diagnostico = 'Normal'
+    }else if (imc > 24.9 && imc <= 29.99){
+        diagnostico = 'Sobrepeso'
+    }else if (imc > 29.99 && imc <= 34.99){
+        diagnostico = 'Obesidade[1]'
+    }else if (imc > 34.99 && imc <=39.99){
+        diagnostico = 'Obesidade[2]'
+    }else{
+        diagnostico = 'Obesidade[3]'
+    }
+    document.getElementById('valor_imc').innerHTML = `<strong>Seu IMC: ${imc.toFixed(2)}    ${diagnostico}</strong>`
+}
+
+
+
+function limpar(){
+    document.getElementById('altura').value = ''
+    document.getElementById('peso').value = ''
+    document.getElementById('valor_imc').innerText = ''
+}
+
+
